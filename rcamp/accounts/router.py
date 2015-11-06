facf1de4250f879af7b0c5d52948900e1999ee11
Zeb Sampedro <sampedro@colorado.edu>
2015-11-06 11:52:29 -0700

    Database router now works with testing framework.

--- rcamp/accounts/router.py
+++ rcamp/accounts/router.py
@@ -1,43 +1,31 @@
+def is_ldap_model(model):
+    return hasattr(model, 'base_dn')
+
 class LdapRouter(object):
     """
-    A router to control all database operations on models in the
-    auth application.
+    A router to point database operations on LDAP models to the one
+    of the LDAP databases.
     """
+
+    def allow_migrate(self, db, model):
+        if is_ldap_model(model):
+            return False
+        return None
+
     def db_for_read(self, model, **hints):
-        """
-        Attempts to read auth models go to auth_db.
-        """
-        if model._meta.object_name in ['RcLdapUser','RcLdapGroup']:
-            return 'rcldap'
-        elif model._meta.object_name in ['CuLdapUser']:
-            return 'culdap'
+        "Point all operations on LDAP models to the appropriate LDAP database"
+        if is_ldap_model(model):
+            if model.startswith('Cu'):
+                return 'culdap'
+            else:
+                return 'rcldap'
         return None
 
     def db_for_write(self, model, **hints):
-        """
-        Attempts to write auth models go to auth_db.
-        """
-        if model._meta.object_name in ['RcLdapUser','RcLdapGroup']:
-            return 'rcldap'
-        elif model._meta.object_name in ['CuLdapUser']:
-            return 'culdap'
-        return None
-
-    def allow_relation(self, obj1, obj2, **hints):
-        """
-        Allow relations if a model in the auth app is involved.
-        """
-        if obj1._meta.app_label == 'ldapbridge' or\
-           obj2._meta.app_label == 'ldapbridge':
-            return True
-        return None
-
-    def allow_migrate(self, db, app_label):
-        """
-        Make sure the auth app only appears in the 'auth_db'
-        database.
-        """
-
-        if db in ['rcldap','culdap']:
-            return False
+        "Point all operations on LDAP models to the appropriate LDAP database"
+        if is_ldap_model(model):
+            if model.startswith('Cu'):
+                return 'culdap'
+            else:
+                return 'rcldap'
         return None
