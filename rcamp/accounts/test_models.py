e5391dfedaf964eca3689978eae76ff0cbeeb7e8
Zeb Sampedro <sampedro@colorado.edu>
2015-11-10 12:55:52 -0700

    Added rcgroup and id tracker test cases.

--- rcamp/accounts/test_models.py
+++ rcamp/accounts/test_models.py
@@ -35,9 +35,23 @@ test_user = (
         'loginShell': ['/bin/bash']
     }
 )
+test_group = (
+    'cn=testgrp,ou=groups,dc=rc,dc=int,dc=colorado,dc=edu', {
+        'objectClass': ['top','posixGroup'],
+        'cn': ['testgrp'],
+        'gidNumber': ['1000'],
+        'memberUid': ['testuser']
+    }
+)
 
+# Base class to set up mock LDAP server for the remainder
+# of the test cases.
+# 
+# In each test that interacts with the mock LDAP, the
+# DATABASE_ROUTERS setting will need to be overridden
+# with the LDAP router for test.
 class BaseCase(TestCase):
-    directory = dict([admin, groups, people, test_user])
+    directory = dict([admin, groups, people, test_user, test_group])
 
     @classmethod
     def setUpClass(cls):
@@ -55,27 +69,87 @@ class BaseCase(TestCase):
         self.mockldap.stop()
         del self.ldapobj
 
-class RcLdapUserTestCase(BaseCase):
+# Ensure basic functionality of the mock LDAP server
+# before continuing onto the remainder of the test suite.
+class MockLdapTestCase(BaseCase):
     @override_settings(DATABASE_ROUTERS=['accounts.router.TestLdapRouter',])
-    def test_read(self):
+    def test_rcuser_read(self):
         u = RcLdapUser.objects.get(username='testuser')
 
-        # self.assertEquals(u.group, 1000)
         self.assertEquals(u.home_directory, '/home/testuser')
         self.assertEquals(u.uid, 1000)
         self.assertEquals(u.username, 'testuser')
         self.assertEquals(u.modified_date, datetime.datetime(2015,11,06,03,43,24))
 
         self.assertRaises(RcLdapUser.DoesNotExist, RcLdapUser.objects.get,
-                          username='does_not_exist')
+                        username='does_not_exist')
 
     @override_settings(DATABASE_ROUTERS=['accounts.router.TestLdapRouter',])
-    def test_update(self):
+    def test_rcuser_update(self):
         u = RcLdapUser.objects.get(username='testuser')
         u.first_name = 'Tested'
         u.save()
         self.assertEquals(u.first_name, 'Tested')
+    
+    @override_settings(DATABASE_ROUTERS=['accounts.router.TestLdapRouter',])
+    def test_rcgroup_read(self):
+        g = RcLdapGroup.objects.get(name='testgrp')
+        
+        self.assertEquals(g.name, 'testgrp')
+        self.assertEquals(g.gid, 1000)
+        self.assertEquals(g.members, ['testuser'])
+        
+        self.assertRaises(RcLdapGroup.DoesNotExist, RcLdapGroup.objects.get,
+                        name='does_not_exist')
+    
+    @override_settings(DATABASE_ROUTERS=['accounts.router.TestLdapRouter',])
+    def test_rcgroup_update(self):
+        g = RcLdapGroup.objects.get(name='testgrp')
+        g.name = 'testedgrp'
+        g.save()
+        self.assertEquals(g.name, 'testedgrp')
 
+# This test case covers IdTracker functionality
+class IdTrackerTestCase(BaseCase):
+    def setUp(self):
+        super(IdTrackerTestCase,self).setUp()
+        idt = IdTracker(
+            category='posix',
+            min_id=1000,
+            max_id=1500,
+            next_id=1001
+        )
+        idt.save()
+    
+    @override_settings(DATABASE_ROUTERS=['accounts.router.TestLdapRouter',])
+    def test_get_next_id(self):
+        idt = IdTracker.objects.get(category='posix')
+        self.assertEquals(idt.next_id, 1001)
+        
+        next_id = idt.get_next_id()
+        self.assertEquals(next_id, 1001)
+        self.assertEquals(idt.next_id, 1002)
+    
+    @override_settings(DATABASE_ROUTERS=['accounts.router.TestLdapRouter',])
+    def test_get_next_id_no_initial_value(self):
+        idt = IdTracker.objects.get(category='posix')
+        idt.next_id = None
+        idt.save()
+        next_id = idt.get_next_id()
+        self.assertEquals(next_id, 1001)
+        self.assertEquals(idt.next_id, 1002)
+    
+    @override_settings(DATABASE_ROUTERS=['accounts.router.TestLdapRouter',])
+    def test_get_next_id_conflict(self):
+        idt = IdTracker.objects.get(category='posix')
+        idt.next_id = 1000
+        idt.save()
+        next_id = idt.get_next_id()
+        self.assertEquals(next_id, 1001)
+        self.assertEquals(idt.next_id, 1002)
+
+# This test case covers creating an LDAP user from
+# a request dictionary.
 class AccountCreationTestCase(BaseCase):
     def setUp(self):
         super(AccountCreationTestCase,self).setUp()
