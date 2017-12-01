7aa5e54613341c71829ac9a8d0f0b8d4aaa02f65
Zebula Sampedro <sampedro@colorado.edu>
2017-12-01 22:54:49 +0000

    Starting to add functional test coverage for project functionality.

--- rcamp/projects/test_functional.py
+++ rcamp/projects/test_functional.py
@@ -7,13 +7,16 @@ from lib.test.functional import UserAuthenticatedLiveServerTestCase
 from projects.models import Project
 
 
-project_dict = dict(
-    pi_emails = ['testpi@test.org'],
-    organization = 'ucb',
-    project_id = 'ucb1',
-    title = 'Test project',
-    description = 'Test project.'
-)
+def get_project_defaults():
+    """Return reasonable defaults for non-m2m project fields."""
+    project_defaults = dict(
+        pi_emails = ['testpi@test.org'],
+        organization = 'ucb',
+        project_id = 'ucb1',
+        title = 'Test project',
+        description = 'Test project.'
+    )
+    return project_defaults
 
 class ProjectDetailsTestCase(UserAuthenticatedLiveServerTestCase):
     def setUp(self):
@@ -24,6 +27,7 @@ class ProjectDetailsTestCase(UserAuthenticatedLiveServerTestCase):
         self.login(username,password)
 
     def test_project_list_as_manager(self):
+        project_dict = get_project_defaults()
         project = Project.objects.create(**project_dict)
         project.managers.add(self.ucb_auth_user)
         project.collaborators.add(self.ucb_auth_user,self.csu_auth_user)
@@ -34,6 +38,7 @@ class ProjectDetailsTestCase(UserAuthenticatedLiveServerTestCase):
         self.assertIn('/projects/create',list_items[1].get_attribute('href'))
 
     def test_project_list_as_suffixed_user(self):
+        project_dict = get_project_defaults()
         project = Project.objects.create(**project_dict)
         project.managers.add(self.csu_auth_user)
         project.collaborators.add(self.csu_auth_user,self.ucb_auth_user)
@@ -49,6 +54,7 @@ class ProjectDetailsTestCase(UserAuthenticatedLiveServerTestCase):
         self.assertIn('/projects/list/{}/'.format(project.pk),list_items[0].get_attribute('href'))
 
     def test_project_details_as_manager(self):
+        project_dict = get_project_defaults()
         project = Project.objects.create(**project_dict)
         project.managers.add(self.ucb_auth_user)
         project.collaborators.add(self.ucb_auth_user,self.csu_auth_user)
@@ -60,6 +66,7 @@ class ProjectDetailsTestCase(UserAuthenticatedLiveServerTestCase):
         request_create_link = self.browser.find_element_by_css_selector('#allocation-request-link')
 
     def test_project_details_as_collaborator(self):
+        project_dict = get_project_defaults()
         project = Project.objects.create(**project_dict)
         project.managers.add(self.csu_auth_user)
         project.collaborators.add(self.csu_auth_user,self.ucb_auth_user)
@@ -78,3 +85,63 @@ class ProjectDetailsTestCase(UserAuthenticatedLiveServerTestCase):
         collaborator_list_td = self.browser.find_element_by_css_selector("#table-collaborator_list-row td:nth-child(2)")
         self.assertIn('testuser@colostate.edu',collaborator_list_td.text)
         self.assertIn('testuser',collaborator_list_td.text)
+
+
+class ProjectCreateTestCase(UserAuthenticatedLiveServerTestCase):
+    def setUp(self):
+        super(ProjectCreateTestCase,self).setUp()
+        # Log in as UCB user
+        username = self.ucb_auth_user.username
+        password = self.ucb_auth_user_dict['password']
+        self.login(username,password)
+
+    def test_create_project(self):
+        project_dict = get_project_defaults()
+        project_dict['pi_emails'].append(self.ucb_auth_user.email)
+
+        self.browser.get(self.live_server_url + '/projects/create')
+
+        # Fill out form
+        organization_option_ucb = self.browser.find_element_by_css_selector('#id_organization > option[value="ucb"]')
+        title_input = self.browser.find_element_by_css_selector('#id_title')
+        description_input = self.browser.find_element_by_css_selector('#id_description')
+        pi_emails_input = self.browser.find_element_by_css_selector('#id_pi_emails')
+        submit_button = self.browser.find_element_by_css_selector('button[type="submit"]')
+        # Manager filter select elements
+        managers_add_link = self.browser.find_element_by_css_selector('#id_managers_add_link')
+        manager_option_ucb_user = self.browser.find_element_by_css_selector('#id_managers_from > option[title="testuser"]')
+        # Collaborators filter select elements
+        collaborators_add_link = self.browser.find_element_by_css_selector('#id_collaborators_add_link')
+        collaborator_option_csu_user = self.browser.find_element_by_css_selector('#id_collaborators_from > option[title="testuser@colostate.edu"]')
+
+        organization_option_ucb.click()
+        title_input.send_keys(project_dict['title'])
+        description_input.send_keys(project_dict['description'])
+        pi_emails_value = ','.join(project_dict['pi_emails'])
+        pi_emails_input.send_keys(pi_emails_value)
+        manager_option_ucb_user.click()
+        managers_add_link.click()
+        collaborator_option_csu_user.click()
+        collaborators_add_link.click()
+
+        submit_button.click()
+        project = Project.objects.get(title=project_dict['title'])
+
+        # Redirected to project details view?
+        self.assertIn('/projects/list/{}/'.format(project.pk),self.browser.current_url)
+        # Verify project attributes
+        self.assertEquals(project.organization,project_dict['organization'])
+        self.assertEquals(project.title,project_dict['title'])
+        self.assertEquals(project.description,project_dict['description'])
+        self.assertEquals(project.pi_emails,project_dict['pi_emails'])
+        self.assertIn(self.ucb_auth_user,project.managers.all())
+        self.assertIn(self.csu_auth_user,project.collaborators.all())
+
+
+class ProjectEditTestCase(UserAuthenticatedLiveServerTestCase):
+    def setUp(self):
+        super(ProjectEditTestCase,self).setUp()
+        # Log in as UCB user
+        username = self.ucb_auth_user.username
+        password = self.ucb_auth_user_dict['password']
+        self.login(username,password)
