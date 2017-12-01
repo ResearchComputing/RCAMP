b6f1347b63ecb77b61427612356876c47c6c0ca0
Zebula Sampedro <sampedro@colorado.edu>
2017-12-01 23:20:18 +0000

    Added some functional coverage for project edit.

--- rcamp/projects/test_functional.py
+++ rcamp/projects/test_functional.py
@@ -117,7 +117,7 @@ class ProjectCreateTestCase(UserAuthenticatedLiveServerTestCase):
         organization_option_ucb.click()
         title_input.send_keys(project_dict['title'])
         description_input.send_keys(project_dict['description'])
-        pi_emails_value = ','.join(project_dict['pi_emails'])
+        pi_emails_value = ', '.join(project_dict['pi_emails'])
         pi_emails_input.send_keys(pi_emails_value)
         manager_option_ucb_user.click()
         managers_add_link.click()
@@ -145,3 +145,60 @@ class ProjectEditTestCase(UserAuthenticatedLiveServerTestCase):
         username = self.ucb_auth_user.username
         password = self.ucb_auth_user_dict['password']
         self.login(username,password)
+
+    def test_edit_project(self):
+        project_dict = get_project_defaults()
+        project_dict['pi_emails'].append(self.ucb_auth_user.email)
+        project = Project.objects.create(**project_dict)
+        project.managers.add(self.ucb_auth_user)
+        project.collaborators.add(self.ucb_auth_user,self.csu_auth_user)
+
+        self.browser.get(self.live_server_url + '/projects/list/{}/edit'.format(project.pk))
+
+        # Get form fields
+        title_input = self.browser.find_element_by_css_selector('#id_title')
+        description_input = self.browser.find_element_by_css_selector('#id_description')
+        pi_emails_input = self.browser.find_element_by_css_selector('#id_pi_emails')
+        submit_button = self.browser.find_element_by_css_selector('button[type="submit"]')
+        # Manager filter select elements
+        managers_remove_link = self.browser.find_element_by_css_selector('#id_managers_remove_link')
+        manager_option_ucb_user = self.browser.find_element_by_css_selector('#id_managers_to > option[title="testuser"]')
+        # Collaborators filter select elements
+        collaborators_remove_link = self.browser.find_element_by_css_selector('#id_collaborators_remove_link')
+        collaborator_option_ucb_user = self.browser.find_element_by_css_selector('#id_collaborators_to > option[title="testuser"]')
+        collaborator_option_csu_user = self.browser.find_element_by_css_selector('#id_collaborators_to > option[title="testuser@colostate.edu"]')
+
+        # Verify form data
+        form_title_value = title_input.get_attribute('value')
+        self.assertEquals(project.title,form_title_value)
+        form_description_value = description_input.get_attribute('value')
+        self.assertEquals(project.description,form_description_value)
+        form_pi_emails_value = pi_emails_input.get_attribute('value')
+        pi_emails_rendered = ','.join(project.pi_emails)
+        self.assertEquals(pi_emails_rendered,form_pi_emails_value)
+
+        # Modify project fields
+        new_pi_emails_value, _ = form_pi_emails_value.split(',')
+        pi_emails_input.clear()
+        pi_emails_input.send_keys(new_pi_emails_value)
+
+        # Erroneously remove yourself as owner. You should be added back in this case.
+        manager_option_ucb_user.click()
+        managers_remove_link.click()
+
+        # Remove a collaborator
+        collaborator_option_csu_user.click()
+        collaborators_remove_link.click()
+
+        submit_button.click()
+        # Redirected to project details view?
+        self.assertIn('/projects/list/{}/'.format(project.pk),self.browser.current_url)
+        project = Project.objects.get(title=project_dict['title'])
+        # Verify project attributes
+        self.assertEquals(project.organization,project_dict['organization'])
+        self.assertEquals(project.title,project_dict['title'])
+        self.assertEquals(project.description,project_dict['description'])
+        self.assertEquals(project.pi_emails,[new_pi_emails_value])
+        self.assertIn(self.ucb_auth_user,project.managers.all())
+        self.assertIn(self.ucb_auth_user,project.collaborators.all())
+        self.assertNotIn(self.csu_auth_user,project.collaborators.all())
