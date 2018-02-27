f8932e1104b97443942c65ae37fc580afc6b46a9
Zebula Sampedro <sampedro@colorado.edu>
2018-02-27 21:42:08 +0000

    Added tests for account request verification views.

--- rcamp/accounts/test_views.py
+++ rcamp/accounts/test_views.py
@@ -50,6 +50,99 @@ def get_org_user_defaults():
     return defaults
 
 
+class AccountRequestVerifyUcbTestCase(LdapTestCase):
+    def setUp(self):
+        idt = IdTracker.objects.create(
+            category='posix',
+            min_id=1000,
+            max_id=1500,
+            next_id=1001
+        )
+
+    def test_request_verify(self):
+        mock_cu_user_defaults = get_org_user_defaults()
+        mock_cu_user = mock.MagicMock(**mock_cu_user_defaults)
+        mock_cu_user.authenticate.return_value = True
+        account_request_verify_defaults = get_account_request_verify_defaults()
+
+        with mock.patch('accounts.models.CuLdapUser.objects.get',return_value=mock_cu_user):
+            mock_cu_user.edu_affiliation = ['affiliated','unrecognized']
+            response = self.client.post(
+                    '/accounts/account-request/create/verify/ucb',
+                    data=account_request_verify_defaults
+                )
+            session_ar_data = self.client.session['account_request_dict']
+
+            self.assertEquals(response.status_code, 302)
+            self.assertTrue(response.url.endswith('/accounts/account-request/create/intent'))
+            self.assertEquals(session_ar_data['organization'],'ucb')
+            self.assertFalse('status' in session_ar_data)
+            self.assertEquals(session_ar_data['email'],'testuser@colorado.edu')
+            self.assertEquals(session_ar_data['role'],'faculty')
+            self.assertEquals(session_ar_data['department'],'physics')
+            self.assertEquals(session_ar_data['first_name'],'Test')
+            self.assertEquals(session_ar_data['last_name'],'User')
+            self.assertEquals(session_ar_data['username'],'testuser')
+
+    def test_request_verify_autoapprove(self):
+        mock_cu_user_defaults = get_org_user_defaults()
+        mock_cu_user = mock.MagicMock(**mock_cu_user_defaults)
+        mock_cu_user.authenticate.return_value = True
+        account_request_verify_defaults = get_account_request_verify_defaults()
+
+        with mock.patch('accounts.models.CuLdapUser.objects.get',return_value=mock_cu_user):
+            mock_cu_user.edu_affiliation = ['faculty']
+            response = self.client.post(
+                    '/accounts/account-request/create/verify/ucb',
+                    data=account_request_verify_defaults
+                )
+            session_ar_data = self.client.session['account_request_dict']
+
+            self.assertEquals(response.status_code, 302)
+            self.assertTrue(response.url.endswith('/accounts/account-request/create/intent'))
+            self.assertEquals(session_ar_data['organization'],'ucb')
+            self.assertEquals(session_ar_data['status'],'a')
+
+    def test_request_verify_invalid_creds(self):
+        mock_cu_user_defaults = get_org_user_defaults()
+        mock_cu_user = mock.MagicMock(**mock_cu_user_defaults)
+        mock_cu_user.authenticate.return_value = True
+        account_request_verify_defaults = get_account_request_verify_defaults()
+
+        with mock.patch('accounts.models.CuLdapUser.objects.get',side_effect=[CuLdapUser.DoesNotExist]):
+            response = self.client.post(
+                    '/accounts/account-request/create/verify/ucb',
+                    data=account_request_verify_defaults
+                )
+
+            self.assertEquals(
+                    response.context['form'].errors['__all__'],
+                    [u'Invalid username']
+                )
+            self.assertRaises(
+                    AccountRequest.DoesNotExist,
+                    AccountRequest.objects.get,
+                    **{'username':account_request_verify_defaults['username']}
+                )
+
+        mock_cu_user.authenticate.return_value = False
+        with mock.patch('accounts.models.CuLdapUser.objects.get',return_value=mock_cu_user):
+            response = self.client.post(
+                    '/accounts/account-request/create/verify/ucb',
+                    data=account_request_verify_defaults
+                )
+
+            self.assertEquals(
+                    response.context['form'].errors['__all__'],
+                    [u'Invalid password']
+                )
+            self.assertRaises(
+                    AccountRequest.DoesNotExist,
+                    AccountRequest.objects.get,
+                    **{'username':account_request_verify_defaults['username']}
+                )
+
+
 class AccountRequestVerifyCsuTestCase(LdapTestCase):
     def setUp(self):
         idt = IdTracker.objects.create(
@@ -59,7 +152,7 @@ class AccountRequestVerifyCsuTestCase(LdapTestCase):
             next_id=1001
         )
 
-    def test_request_create_csu(self):
+    def test_request_verify(self):
         mock_csu_user_defaults = get_org_user_defaults()
         mock_csu_user = mock.MagicMock(**mock_csu_user_defaults)
         mock_csu_user.authenticate.return_value = True
@@ -77,7 +170,7 @@ class AccountRequestVerifyCsuTestCase(LdapTestCase):
             self.assertEquals(session_ar_data['organization'],'csu')
             self.assertEquals(session_ar_data['status'],'a')
 
-    def test_request_create_invalid_creds(self):
+    def test_request_verify_invalid_creds(self):
         mock_csu_user_defaults = get_org_user_defaults()
         mock_csu_user = mock.MagicMock(**mock_csu_user_defaults)
         mock_csu_user.authenticate.return_value = True
