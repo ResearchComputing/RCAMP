ec54e70b2c4e42bdff369344046b482e2d2ca414
Zebula Sampedro <sampedro@colorado.edu>
2018-03-28 21:26:42 +0000

    Added a less-than-ideal, but workable fix for #290.

--- rcamp/mailer/receivers.py
+++ rcamp/mailer/receivers.py
@@ -11,6 +11,15 @@ def notify_account_request_received(sender, **kwargs):
         ctx = {'account_request':account_request}
         msg = notifier.send(context=ctx)
 
+@receiver(account_request_approved)
+def notify_account_request_approved(sender, **kwargs):
+    account_request = kwargs.get('account_request')
+
+    notifiers = MailNotifier.objects.filter(event='account_request_approved')
+    for notifier in notifiers:
+        ctx = {'account_request':account_request}
+        msg = notifier.send(context=ctx)
+
 @receiver(account_created_from_request)
 def notify_account_created_from_request(sender, **kwargs):
     account = kwargs.get('account')
--- rcamp/mailer/signals.py
+++ rcamp/mailer/signals.py
@@ -2,6 +2,7 @@ import django.dispatch
 
 
 account_request_received = django.dispatch.Signal(providing_args=['account_request'])
+account_request_approved = django.dispatch.Signal(providing_args=['account_request'])
 account_created_from_request = django.dispatch.Signal(providing_args=['account'])
 
 project_created_by_user = django.dispatch.Signal(providing_args=['project'])
