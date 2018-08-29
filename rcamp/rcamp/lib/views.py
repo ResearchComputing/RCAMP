81a0ecf1ce939912d471ab64e2e992f434f1feca
Zebula Sampedro <sampedro@colorado.edu>
2018-08-29 15:00:51 -0600

    Issues/307 (#331)
    * Starting to redesign user-facing pages.
    
    * Redirect authed users to project management page. Closes #310, closes #308.
    
    * Revised placement and content on jumbotrons. Closes #306.
    
    * Finished minimizing jumbotron use and content. Closes #307.

--- rcamp/rcamp/lib/views.py
+++ rcamp/rcamp/lib/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.shortcuts import redirect
 
 
 def handler404(request):
@@ -13,4 +14,12 @@ def handler500(request):
     response = render_to_response('500.html', {},
                                   context_instance=RequestContext(request))
     response.status_code = 500
+    return response
+
+def index_view(request):
+    if request.user.is_authenticated():
+        return redirect('projects:project-list')
+
+    context_instance = RequestContext(request)
+    response = render_to_response('index.html', {}, context_instance=context_instance)
     return response
