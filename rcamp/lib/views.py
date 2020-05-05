d8a87e50fa5b79cb374ae74784a7ad6526cddf29
AaronTHolt <aaron.holt@colorado.edu>
2020-05-05 07:07:59 -0600

    Fix context_instance deprecation

--- rcamp/lib/views.py
+++ rcamp/lib/views.py
@@ -1,25 +1,18 @@
 from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import redirect
 
 
 def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
+    return render(request, '404.html', {}, status=404)
 
 
 def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
+    return render(request, '500.html', {}, status=500)
 
 def index_view(request):
     if request.user.is_authenticated():
         return redirect('projects:project-list')
 
-    context_instance = RequestContext(request)
-    response = render_to_response('index.html', {}, context_instance=context_instance)
-    return response
+    return render(request, 'index.html', {})
