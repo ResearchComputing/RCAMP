7a60c73781bc7f6bd906efd38d8647a775a14aca
AaronTHolt <aaron.holt@colorado.edu>
2020-05-20 08:04:39 -0600

    Use staticfiles_urlpatterns() to serve static files in dev

--- rcamp/rcamp/urls.py
+++ rcamp/rcamp/urls.py
@@ -41,6 +41,10 @@ urlpatterns = [
     url(r'^projects/', include('projects.urls', namespace='projects')),
 ]
 
+
 if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
