3dfbf599a36b07daa40dbf261bb56dc22b4d56ab
aarontholt <aaron.holt@colorado.edu>
2020-08-31 16:19:22 -0600

    Switch to new-style admin url pattern include

--- rcamp/rcamp/urls.py
+++ rcamp/rcamp/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     url(r'^$', index_view, name='index'),
     url(r'^login', auth_views.login, {'template_name':'login.html'}),
     url(r'^logout', auth_views.logout, {'template_name':'logout.html'}),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^api/', include('endpoints.urls')),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^projects/', include('projects.urls', namespace='projects')),
