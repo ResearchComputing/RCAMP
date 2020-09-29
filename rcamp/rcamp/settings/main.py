a3dece62703e04383475629eece7b131baf18bee
aarontholt <aaron.holt@colorado.edu>
2020-09-29 09:14:57 -0600

    Upgrade Django version to 2.0 Middleware_Classes is deprecated for Middleware

--- rcamp/rcamp/settings/main.py
+++ rcamp/rcamp/settings/main.py
@@ -34,16 +34,15 @@ INSTALLED_APPS = [
 if DEBUG:
     INSTALLED_APPS.append('tests')
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-)
+]
 
 ROOT_URLCONF = 'rcamp.urls'
 
