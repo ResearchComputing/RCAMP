dbe3ad30b1499c877be923d648c478d926277a0b
aarontholt <aaron.holt@colorado.edu>
2021-04-29 15:55:02 -0600

    Fix cu-ldap test ldap password for dev env

--- rcamp/tests/test_lib_auth.py
+++ rcamp/tests/test_lib_auth.py
@@ -2,6 +2,11 @@ from django.conf import settings
 import mock
 import pam
 from lib.pam_backend import PamBackend
+from lib.ldap_backend import (
+    AUTH_UCB_LDAP_Backend,
+    AUTH_RC_LDAP_Backend,
+    AUTH_CSU_LDAP_Backend
+)
 from tests.utilities.ldap import (
     LdapTestCase,
     get_ldap_user_defaults
@@ -77,4 +82,20 @@ class PamBackendTestCase(LdapTestCase):
         self.assertEqual(user.username, 'testuser')
 
 
-# class LDAPBackendTestCase(LdapTestCase):
+class AUTH_UCB_LDAPBackendTestCase(LdapTestCase):
+    def setUp(self):
+        self.ucb_ldap_backend = AUTH_UCB_LDAP_Backend()
+        super(AUTH_UCB_LDAPBackendTestCase, self).setUp()
+
+    def test_authenticate(self):
+        rc_user_defaults = get_ldap_user_defaults()
+        RcLdapUser.objects.create(organization='ucb',**rc_user_defaults)
+        rc_user = RcLdapUser.objects.get(username='testuser')
+        self.assertRaises(User.DoesNotExist, User.objects.get,
+                        username='testuser')
+        user = self.ucb_ldap_backend.authenticate(None, username='testuser',password='passwd')
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username,rc_user.username)
+        self.assertEqual(user.first_name,rc_user.first_name)
+        self.assertEqual(user.last_name,rc_user.last_name)
+        self.assertEqual(user.email,rc_user.email)
